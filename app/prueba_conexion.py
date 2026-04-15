0aa1644aa126541da1ebee5d3a2b3fa3258b3dec
Diego Turconi <111567535+dturconi17@users.noreply.github.com>
2026-04-15 13:15:59 -0300

    Remove sensitive database
    Removed sensitive database connection details from the script.

--- app/prueba_conexion.py
+++ app/prueba_conexion.py
@@ -1,10 +1,6 @@
 import pyodbc
 import pandas as pd
 
-server = '148.113.152.56'
-database = 'SISVITA'
-username = 'consulta_ventas'
-password = '.3e1JN$x[k7eB>UC<NTZ'
 
 conn = pyodbc.connect(
     'DRIVER={ODBC Driver 17 for SQL Server};'
